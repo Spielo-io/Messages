68d2930e08ce94dd7a4faf7929f5e96ad4f1285e
Marc Pfister <pfister.marc-it19@it.dhbw-ravensburg.de>
2021-02-09 09:55:33 +0100

    Fixed bug with with parsing bytes to long

--- src/io/spielo/util/BufferHelper.java
+++ src/io/spielo/util/BufferHelper.java
@@ -31,21 +31,21 @@ public class BufferHelper {
 	}
 	
 	public static short fromBufferIntoShort(final byte[] buffer, final int offset) {
-		return (short)(buffer[offset] | buffer[offset + 1] << 8);
+		return (short)(buffer[offset] & 0xFF | buffer[offset + 1] & 0xFF << 8);
 	}
 
 	public static int fromBufferIntoInt(final byte[] buffer, final int offset) {
-		return buffer[offset] | 
-			(buffer[offset + 1] <<  8) |  
-			(buffer[offset + 2] << 16) | 
-			(buffer[offset + 3] << 24); 
+		return buffer[offset] & 0xFF | 
+			(buffer[offset + 1] & 0xFF <<  8) |  
+			(buffer[offset + 2] & 0xFF << 16) | 
+			(buffer[offset + 3] & 0xFF << 24); 
 	}
 
 	public static long fromBufferIntoLong(final byte[] buffer, final int offset) {
 		long value = 0;
 		int shift = 0;
 		for (int i = 0; i < 8; i++) {
-			value |= (long)(buffer[offset + i] << shift);
+			value |= ((long)(buffer[offset + i] & 0xFF) << shift);
 			shift += 8;
 		}
 		return value;
