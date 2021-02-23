a39cf6a4e3607d7db802dc65a3e42f9be9514cd8
Andreas <andilengl@outlook.de>
2021-02-23 15:06:59 +0100

    Add LobbyCreateResponse

--- src/io/spielo/messages/CreateLobbyResponseMessage.java
+++ src/io/spielo/messages/CreateLobbyResponseMessage.java
@@ -1,2 +1,24 @@
-package io.spielo.messages;public class CreateLobbyResponseMessage {
+package io.spielo.messages;
+
+import io.spielo.messages.util.BufferBuilder;
+
+import java.nio.charset.StandardCharsets;
+
+public class CreateLobbyResponseMessage extends Message {
+    private final String code;
+
+    public CreateLobbyResponseMessage(final MessageHeader header, final String code) {
+        super(header);
+        this.code = code;
+    }
+
+    @Override
+    protected short getBodyLength() {
+        return (short)this.code.getBytes(StandardCharsets.UTF_8).length;
+    }
+
+    @Override
+    protected void bodyIntoBuffer(BufferBuilder builder) {
+        builder.addString(this.code);
+    }
 }
