a5b49640a1835c21e26d028469bd4bdc120ef8ed
Marc Pfister <pfister.marc-it19@it.dhbw-ravensburg.de>
2021-02-26 21:24:45 +0100

    Implement MessageType2Game enum

--- src/io/spielo/messages/types/MessageType2Game.java
+++ src/io/spielo/messages/types/MessageType2Game.java
@@ -1,12 +1,15 @@
 package io.spielo.messages.types;
 
 public enum MessageType2Game implements ByteEnum {
-    TEST((byte) 0);
+    TicTacToe(0),
+    Win4(1),
+    Mill(2),
+    Checkers(3);
 	
     private final byte b;
 
-    MessageType2Game(final byte b) {
-        this.b = b;
+    MessageType2Game(final int b) {
+        this.b = (byte) b;
     }
 
     @Override
